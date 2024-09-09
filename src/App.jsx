1bbeac5a1855cf0db81ac5d3e4602b3bf370948a
Krish-Panchani <krishpanchani1346@gmail.com>
2024-09-09 19:07:12 +0530

    Refactor component imports and add new routes
    This commit refactors the component imports in the App.jsx file to use the correct paths. It also adds new routes for the Admin and EmployeeLoginPage components. This change ensures that the application can navigate to these new pages correctly.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+// src/App.js
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import TrackLocation from './components/TrackLocation';
 import AboutPage from './pages/AboutPage';
 import NotFoundPage from './pages/NotFoundPage';
-import LocationTracker from './components/LocationTracker';
+import LocationTracker from './pages/LocationTracker';
+import Admin from './pages/Admin';
+import EmployeeLoginPage from './pages/EmployeeLoginPage';
 
 const App = () => {
   return (
@@ -15,6 +18,8 @@ const App = () => {
         <Route path="/track-location" element={<TrackLocation />} />
         <Route path="/location" element={<LocationTracker />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/employee-login" element={<EmployeeLoginPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>

ab4dde18a4e6474940a527cf9a4bda1582f8ffa9
Krish-Panchani <krishpanchani1346@gmail.com>
2024-09-09 19:08:34 +0530

    Refactor SignOut component to use Firebase authentication for user sign out

---
+++ src/components/Admin/SignOut.jsx
@@ -0,0 +1,27 @@
+// import React from 'react';
+import { auth } from '../../firebase';
+import { MdLogout } from "react-icons/md";
+
+const SignOut = () => {
+
+    const handleSignOut = async () => {
+        try {
+            await auth.signOut();
+            console.log("User signed out successfully");
+        } catch (error) {
+            console.error("Error signing out: ", error);
+        }
+    };
+
+    return (
+
+        <button
+            onClick={handleSignOut}
+            className='flex items-center gap-2 px-4 py-2 bg-red-500 rounded-full text-white font-semibold'>
+            <MdLogout className='text-xl' />
+            <span>Sign Out</span>
+        </button>
+    );
+};
+
+export default SignOut;

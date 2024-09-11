a5084c6bb0df6fe784650529196dca3f49d4e173
Krish-Panchani <krishpanchani1346@gmail.com>
2024-09-11 19:02:55 +0530

    Refactor Admin component to update user authentication and add functionality to add office and user

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,69 +1,53 @@
-// src/pages/Admin.js
-
 import { useState, useEffect } from 'react';
-import Login from '../components/Admin/Login';
+import Login from '../components/Login';
 import useAuth from '../hooks/useAuth';
 import SignOut from '../components/Admin/SignOut';
 import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../firebase'; // Ensure you have your Firestore instance
+import { db } from '../firebase'; 
 import AddOffice from '../components/Admin/AddOffice';
+import AddUser from '../components/Admin/AddUser';
 
 export default function Admin() {
   const user = useAuth();
   const [isAdmin, setIsAdmin] = useState(false);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const checkAdminStatus = async () => {
+    const checkAdmin = async () => {
       if (user) {
-        const userDocRef = doc(db, 'admins', user.uid);
+        const userDocRef = doc(db, 'users', user.uid);
         const docSnap = await getDoc(userDocRef);
-
         if (docSnap.exists()) {
-          setIsAdmin(true);
-        } else {
-          setIsAdmin(false);
+          setIsAdmin(docSnap.data().isAdmin === true);
         }
-      } else {
-        setIsAdmin(false); // If no user is logged in, they can't be an admin
       }
-      setLoading(false);
-    };
-
-    checkAdminStatus();
+    }
+    checkAdmin();
   }, [user]);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  console.log(user);
-  console.log(isAdmin);
-
+  console.log("Admin: ", isAdmin);
+  
+  
   return (
     <div>
-      
-      {user ? (
-        isAdmin ? (
+      {isAdmin ? (
           <>
-          <div className='flex items-center justify-between p-6'>
-          <div className='flex items-center gap-4'>
-            <img src={user.photoURL} alt="Profile" className='rounded-full w-10'  />
-            <p><span className='font-semibold'>{user.displayName}</span></p>
-            </div>
-            <SignOut />
+            <div className='flex items-center justify-between p-6'>
+              <div className='flex items-center gap-4'>
+                <img src={user.photoURL} alt="Profile" className='rounded-full w-10' />
+                <p><span className='font-semibold'>{user.displayName}</span></p>
+              </div>
+              <SignOut />
             </div>
             <AddOffice user={user} />
+            <AddUser user={user} />
           </>
-        ) : (
-          <p>Only Admins can access this page.</p>
-        )
-      ) : (
+        )  : (
         <>
-        <h1>ADMIN ACCESS ONLY</h1>
-        <Login />
+          <h1>ADMIN ACCESS ONLY</h1>
+          <Login />
         </>
       )}
     </div>
+    
   );
 }

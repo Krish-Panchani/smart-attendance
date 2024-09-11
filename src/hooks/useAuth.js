0d4be8fa2ba3da93c242637fdcd3a1377a310cf0
Krish-Panchani <krishpanchani1346@gmail.com>
2024-09-11 18:57:58 +0530

    Refactor useAuth hook to include user role and office information

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,22 +1,32 @@
-// src/hooks/useAuth.js
-import { useEffect, useState } from 'react';
-import { getAuth } from 'firebase/auth';
+import { useState, useEffect } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
+import { db as firestore } from '../firebase';
 
 const useAuth = () => {
   const [user, setUser] = useState(null);
   const auth = getAuth();
+  const db = getFirestore();
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        setUser(user);
+    const unsubscribe = onAuthStateChanged(auth, async (authUser) => {
+      if (authUser) {
+        const userDocRef = doc(firestore, 'users', authUser.uid); // Assuming email is used as ID
+        const docSnap = await getDoc(userDocRef);
+        console.log("doc snap: ", docSnap.data());
+
+        if (docSnap.exists()) {
+          setUser({ ...authUser, role: docSnap.data().role, officeId: docSnap.data().officeId, userCode: docSnap.data().userCode });
+        } else {
+          setUser({ ...authUser, role: 'GUEST' }); // Default role
+        }
       } else {
         setUser(null);
       }
     });
 
     return () => unsubscribe();
-  }, [auth]);
+  }, [auth, db]);
 
   return user;
 };

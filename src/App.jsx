12d00e1ae3a5ced687ec66b3f58c87224b5ca3f6
Krish-Panchani <krishpanchani1346@gmail.com>
2024-09-11 19:02:47 +0530

    Refactor App component to update routes and add private route functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,36 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
-import TrackLocation from './components/TrackLocation';
+import TrackLocation from './Testing_Comp/TrackLocation';
 import AboutPage from './pages/AboutPage';
 import NotFoundPage from './pages/NotFoundPage';
 import LocationTracker from './pages/LocationTracker';
 import Admin from './pages/Admin';
-import EmployeeLoginPage from './pages/EmployeeLoginPage';
+import AddEmploee from './components/Admin/AddEmployee';
+import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import useAuth from './hooks/useAuth';
+
+const PrivateRoute = ({ element, allowedRoles }) => {
+  const user = useAuth();
+  const userRole = user?.role; // Ensure role is fetched from the user's profile
+
+  if (!user) {
+    return <Navigate to="/employee-login" />;
+  }
+
+  if (allowedRoles && !allowedRoles.includes(userRole)) {
+    return <Navigate to="/" />;
+  }
+
+  return element;
+};
+
+// Define PropTypes for the PrivateRoute component
+PrivateRoute.propTypes = {
+  element: PropTypes.element.isRequired,
+  allowedRoles: PropTypes.arrayOf(PropTypes.string),
+};
 
 const App = () => {
   return (
@@ -19,8 +43,9 @@ const App = () => {
         <Route path="/location" element={<LocationTracker />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/admin" element={<Admin />} />
-        <Route path="/employee-login" element={<EmployeeLoginPage />} />
+        <Route path="/add-employee" element={<AddEmploee />} />
         <Route path="*" element={<NotFoundPage />} />
+      
       </Routes>
     </Router>
   );

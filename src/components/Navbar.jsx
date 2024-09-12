13559fd417b50d2d233576884fc0d2e2b261b896
Krish-Panchani <krishpanchani1346@gmail.com>
2024-09-12 17:28:53 +0530

    Refactor Navbar component to add admin functionality and display user profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,64 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import SignOut from './SignOut';
+import useAuth from '../hooks/useAuth';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../firebase';
+import Login from './Login';
 
 const Navbar = () => {
+  const user = useAuth(); 
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    const checkAdmin = async () => {
+      if (user) {
+        const userDocRef = doc(db, 'users', user.uid);
+        const docSnap = await getDoc(userDocRef);
+        if (docSnap.exists()) {
+          setIsAdmin(docSnap.data().isAdmin === true);
+        }
+      }
+    }
+    checkAdmin();
+  }, [user]);
+
   return (
-    <nav className="bg-blue-500 p-4">
-      <ul className="flex space-x-4 justify-center text-white">
-        <li>
-          <Link to="/" className="hover:underline">Home</Link>
-        </li>
-        <li>
-          <Link to="/track-location" className="hover:underline">Track Location</Link>
-        </li>
-        <li>
-          <Link to="/about" className="hover:underline">About</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="bg-blue-500 p-4">
+        <ul className="flex space-x-4 justify-center text-white">
+          <li>
+            <Link to="/" className="hover:underline">Home</Link>
+          </li>
+          <li>
+            <Link to="/location" className="hover:underline">Track Location</Link>
+          </li>
+          <li>
+            <Link to="/about" className="hover:underline">About</Link>
+          </li>
+          {
+            isAdmin && (
+              <li>
+                <Link to="/admin" className="hover:underline">Admin</Link>
+              </li>
+            )
+          }
+        </ul>
+      </nav>
+      {user ? (
+        <div className='flex items-center justify-between p-6'>
+          <div className='flex items-center gap-4'>
+            <img src={user.photoURL} alt="Profile" className='rounded-full w-10' />
+            <p><span className='font-semibold'>{user.displayName}</span></p>
+            <p>({user.userCode})</p>
+          </div>
+          <SignOut />
+        </div>
+      )
+    : (
+      <Login />
+    )}
+    </>
   );
 };
 

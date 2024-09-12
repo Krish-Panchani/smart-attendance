0951ec79d951894acbbddf92c2cf599cb026b29c
Krish-Panchani <krishpanchani1346@gmail.com>
2024-09-12 17:47:01 +0530

    Refactor LocationTracker component to conditionally render content based on user authentication status

--- src/pages/LocationTracker.jsx
+++ src/pages/LocationTracker.jsx
@@ -2,16 +2,32 @@ import  useLocationTracker from '../hooks/useLocationTracker';
 import LocationStatus from '../components/LocationStatus';
 import TodayOfficeTime from '../components/TodayOfficeTime';
 import TodayLogs from '../components/TodayLogs';
+import useAuth from '../hooks/useAuth';
 
 const LocationTracker = () => {
   const { status, distance, logs, effectiveTime } = useLocationTracker();
+  const user = useAuth();
 
   return (
-    <div className="p-6 max-w-3xl mx-auto bg-white shadow-md rounded-lg">
-      <LocationStatus status={status} distance={distance || 'Distance unknown'} />
-      <TodayOfficeTime logs={logs} effectiveTime={effectiveTime} />
-      <TodayLogs logs={logs} />
-    </div>
+    <>
+    {
+      user ? (
+        <div className="p-6 max-w-3xl mx-auto bg-white shadow-md rounded-lg">
+        <LocationStatus status={status} distance={distance || 'Distance unknown'} />
+        <TodayOfficeTime logs={logs} effectiveTime={effectiveTime} />
+        <TodayLogs logs={logs} />
+      </div>
+      )
+      :
+      (
+        <div className="p-6 max-w-3xl mx-auto bg-white shadow-md rounded-lg">
+          <p className="text-xl text-center text-red-600 font-semibold">Please login to track your location</p>
+        </div>
+      )
+    }
+
+    </>
+    
   );
 };
 
